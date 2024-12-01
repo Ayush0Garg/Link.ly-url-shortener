781bc90cf32e4783980c61d308d59159c0480160
Ayush0Garg <109014313+Ayush0Garg@users.noreply.github.com>
2024-12-01 18:54:49 +0530

    Update UrlRepo.java

--- src/main/java/com/Linkly/Repository/UrlRepo.java
+++ src/main/java/com/Linkly/Repository/UrlRepo.java
@@ -1,9 +1,11 @@
-package com.Linkly.Repository;
+package com.example.Linkly.Repository;
 
-import com.Linkly.Models.UrlModel;
+import com.example.Linkly.Models.UrlModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,10 @@ public interface UrlRepo extends JpaRepository<UrlModel, Long> {
 
     Optional<UrlModel> findByshortUrl(String shortUrl);
     Optional<UrlModel> findByoriginalUrl(String originalUrl);
+
+    @Query(value = "DELETE FROM url_model WHERE expiration_date > CURRENT_DATE", nativeQuery = true)
+    void removeExpiredLinks();
+    @Query(value = "Select * from url_model order by count_clicked DESC", nativeQuery = true)
+    List<UrlModel> getAllLinksByCountClicked ();
+
 }
