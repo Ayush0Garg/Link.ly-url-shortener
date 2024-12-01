6904f512691780036a8fb47e3db4f5fd26ba8946
Ayush0Garg <109014313+Ayush0Garg@users.noreply.github.com>
2024-12-01 18:57:31 +0530

    Update UrlController.java
    added expiration feature, changed size of original url to max 255, added most clicked sites feature

--- src/main/java/com/Linkly/Controllers/UrlController.java
+++ src/main/java/com/Linkly/Controllers/UrlController.java
@@ -1,12 +1,14 @@
-package com.Linkly.Controllers;
-import com.Linkly.Models.CustomUrlModel;
-import com.Linkly.Models.UrlModel;
-import com.Linkly.Services.UrlService;
+package com.example.Linkly.Controllers;
+import com.example.Linkly.Models.CustomUrlModel;
+import com.example.Linkly.Models.UrlModel;
+import com.example.Linkly.Services.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +20,10 @@ public class UrlController {
 
     @PostMapping("/shorten")
     public ResponseEntity<String> urlShortener(@RequestBody UrlModel urlModel){
+        if (urlModel.getOriginalUrl().length() > 255) {
+            urlModel.setOriginalUrl(urlModel.getOriginalUrl().substring(0, 255));
+        }
+
         String shortUrl = urlService.urlShortener(urlModel.getOriginalUrl());
         return ResponseEntity.ok(shortUrl);
     }
@@ -30,7 +36,7 @@ public class UrlController {
 
     @GetMapping("/{shortUrl}")
     public RedirectView redirectUrl(@PathVariable String shortUrl){
-        String originalUrl = urlService.findUrl(shortUrl);
+        String originalUrl = urlService.findUrlredirect(shortUrl);
         return new RedirectView(originalUrl);
     }
 
@@ -38,6 +44,13 @@ public class UrlController {
     public ResponseEntity<String> customUrl(@RequestBody CustomUrlModel customUrlModel){
         String newUrl = urlService.customUrl(customUrlModel);
         return ResponseEntity.ok(newUrl);
+    }
+
+    @GetMapping("/get/mostClicked")
+    ResponseEntity<List<UrlModel>> getMostClicked(){
+        List<UrlModel> list ;
+        list = urlService.getAllLinks();
+        return ResponseEntity.ok(list);
     }
 
 
